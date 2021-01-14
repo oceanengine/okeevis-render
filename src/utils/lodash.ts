f3463d0f4f1da2a82e29b2ef788af615d1f6ad1e
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-14 22:39:25 +0800

    remove unused lodash function

--- src/utils/lodash.ts
+++ src/utils/lodash.ts
@@ -12,40 +12,16 @@ import isObject = require('lodash/isObject');
 import min = require('lodash/min');
 import max = require('lodash/max');
 import isArray = require('lodash/isArray');
-import shuffle = require('lodash/shuffle');
 import findIndex = require('lodash/findIndex');
 import find = require('lodash/find');
-import merge = require('lodash/merge');
 import last = require('lodash/last');
 import first = require('lodash/first');
-import inRange = require('lodash/inRange');
 import includes = require('lodash/includes');
 import clamp = require('lodash/clamp');
 import pick = require('lodash/pick');
 import omit = require('lodash/omit');
-import assign = require('lodash/assign');
-import throttle = require('lodash/throttle');
-import template = require('lodash/template');
-import set = require('lodash/set');
-import get = require('lodash/get');
 import toNumber = require('lodash/toNumber');
-import mergeWith = require('lodash/mergeWith');
 import flatten = require('lodash/flatten');
-import map = require('lodash/map');
-import filter = require('lodash/filter');
-import uniq = require('lodash/uniq');
-import orderBy = require('lodash/orderBy');
-import sum = require('lodash/sum');
-import trim = require('lodash/trim');
-// import keys = require('lodash/keys');
-import intersection = require('lodash/intersection');
-import difference = require('lodash/difference');
-import sortBy = require('lodash/sortBy');
-// import root = require('lodash/_root')
-
-// if (typeof root !== 'undefined' && !root.Date) {
-//   root.Date = Date;
-// }
 
 export {
   isNull,
@@ -58,33 +34,14 @@ export {
   min,
   max,
   isArray,
-  shuffle,
   findIndex,
   find,
   last,
   first,
-  inRange,
   includes,
   clamp,
   pick,
   omit,
-  throttle,
-  template,
-  set,
-  get,
   toNumber,
-  assign,
-  merge,
-  mergeWith,
   flatten,
-  map,
-  filter,
-  uniq,
-  orderBy,
-  sum,
-  trim,
-  // keys,
-  intersection,
-  difference,
-  sortBy,
 };
