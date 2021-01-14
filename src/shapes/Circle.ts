dd3838e54e01cc72662710b136d7446b984b2c1e
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-14 14:21:35 +0800

    circle pick

--- src/shapes/Circle.ts
+++ src/shapes/Circle.ts
@@ -13,6 +13,8 @@ const shapeKeys: Array<keyof CircleConf> = ['cx', 'cy', 'radius'];
 export default class Circle extends Shape<CircleConf> {
   public type = 'circle';
 
+  public pickByGPU = false;
+
   public shapeKeys = shapeKeys;
   
   public getDefaultAttr(): CircleConf {
