ffeb7669dcd2d13d019c7c8ed3242831f43badb8
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-15 16:05:29 +0800

    remove unused ease

--- src/animate/ease.ts
+++ src/animate/ease.ts
@@ -1,5 +1,6 @@
+/* eslint-disable */
 
-export type EasingName = keyof (typeof all);
+export type EasingName = keyof (typeof easing);
 export type EasingFunction = (k: number) => number;
 const easing = {
   // 线性
@@ -349,62 +350,4 @@ const easing = {
   },
 };
 
-const all = {
-  ...easing,
-  // no easing, no acceleration
-  linear: (t: number): number => {
-      return t;
-  },
-  // accelerating from zero velocity
-  easeInQuad: (t: number): number => {
-      return t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuad: (t: number): number => {
-      return t * (2 - t);
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuad: (t: number): number => {
-      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-  },
-  // accelerating from zero velocity
-  easeInCubic: (t: number): number => {
-      return t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutCubic: (t: number): number => {
-      return --t * t * t + 1;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutCubic: (t: number): number => {
-      return t < 0.5
-          ? 4 * t * t * t
-          : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-  },
-  // accelerating from zero velocity
-  easeInQuart: (t: number): number => {
-      return t * t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuart: (t: number): number => {
-      return 1 - --t * t * t * t;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuart: (t: number): number => {
-      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
-  },
-  // accelerating from zero velocity
-  easeInQuint: (t: number): number => {
-      return t * t * t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuint: (t: number): number => {
-      return 1 + --t * t * t * t * t;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuint: (t: number): number => {
-      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
-  },
-};
-
-export default all;
+export default easing;
