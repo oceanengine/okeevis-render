a4cc0e86304784fdab3e53b8f088805c049dddec
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-15 18:56:46 +0800

    update all optimize

--- src/shapes/Group.ts
+++ src/shapes/Group.ts
@@ -177,6 +177,11 @@ export default class Group<T extends Element = Element> extends Element<GroupCon
       return;
     }
 
+    if (list.length === 0) {
+      this.clear();
+      return;
+    }
+
     const result = diff(prevList, list, (item, index) => {
       const attr = item.attr;
       const key = attr.key ? (item.type + attr.key) : `auto-key-${item.type}-${index}`;
