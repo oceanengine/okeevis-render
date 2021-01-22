7c8ecd90686c134ada79199f364fdf01591d92e3
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-22 16:22:10 +0800

    use native method

--- src/interpolate/index.ts
+++ src/interpolate/index.ts
@@ -1,23 +1,21 @@
-
 import * as lodash from '../utils/lodash';
 
 import interpolateNumber from './interpolateNumber';
 import interpolateArray from './interpolateArray';
 import interpolateObject from './interpolateObject';
 
-
-export function interpolate<T=any>(from: T, to: T, k: number): T {
-  
-  if (lodash.isNumber(from) && lodash.isNumber(to)) {
-    return interpolateNumber(from, to, k) as any as T;
-  } if (lodash.isArray(from) && lodash.isArray(to)) {
-    return interpolateArray(from, to, k) as any as T;
-  } if (lodash.isObject(from) && lodash.isObject(to)) {
+export function interpolate<T = any>(from: T, to: T, k: number): T {
+  if (typeof from === 'number' && typeof to === 'number') {
+    return (interpolateNumber(from, to, k) as any) as T;
+  }
+  if (Array.isArray(from) && Array.isArray(to)) {
+    return (interpolateArray(from as any[], to as any[], k) as any) as T;
+  }
+  if (lodash.isObject(from) && lodash.isObject(to)) {
     return interpolateObject(from, to, k);
   }
 
   return to;
-  
 }
 
-export type InterpolateFunction<T=any> = (from: T, to: T, k: number) => T;
+export type InterpolateFunction<T = any> = (from: T, to: T, k: number) => T;
