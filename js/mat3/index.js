1eb386db6591cb26589fd76e48d570ae76e5eb5d
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-26 16:47:44 +0800

    mat3 create for ie9

--- js/mat3/index.js
+++ js/mat3/index.js
@@ -1,1 +1,22 @@
-module.exports = require('gl-matrix/mat3');
+var mat3 = require('gl-matrix/mat3');
+
+mat3.create = function create() {
+  var out;
+  if (typeof Float32Array !== undefined) {
+    out = new Float32Array(9);
+  } else {
+    out = new Array(9);
+  }
+  out[1] = 0;
+  out[2] = 0;
+  out[3] = 0;
+  out[5] = 0;
+  out[6] = 0;
+  out[7] = 0;
+  out[0] = 1;
+  out[4] = 1;
+  out[8] = 1;
+  return out;
+};
+
+module.exports = mat3;
