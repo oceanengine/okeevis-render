23a3022b6ca5f8e119d9143247840832acffd657
liwensheng.001 <liwensheng.001@bytedance.com>
2021-01-26 18:38:51 +0800

    download method

--- src/render.ts
+++ src/render.ts
@@ -8,6 +8,7 @@ import {getDomContentSize, } from './utils/dom';
 import requestAnimationFrame from './utils/requestAnimationFrame';
 import { getPainter } from './painter';
 import { renderToSVGString } from './svg/renderToSVGString';
+import {  downloadBase64, } from './utils/download';
 
 import './painter/CanvasPainter';
 import './painter/SVGPainter';
@@ -165,32 +166,7 @@ export default class Render extends EventFul {
 
   public downloadImage(name: string): void {
     const base64: string = this.getPainter().getBase64();
-    const ismsie = !!(window as any).ActiveXObject || 'ActiveXObject' in window
-    const isedge = navigator.userAgent.indexOf('Edge') > -1;
-    const a: HTMLAnchorElement = document.createElement('a');
-    a.href = base64; // 将画布内的信息导出为png图片数据
-    a.download = name || document.title; // 设定下载名称
-    a.target = '_blank';
-    // for Chrome and Firefox
-    // 来自echarts
-    if (
-        typeof MouseEvent === 'function' &&
-        !(ismsie || isedge)
-    ) {
-        const evt: MouseEvent = new MouseEvent('click', {
-            view: window,
-            bubbles: true,
-            cancelable: false,
-        });
-        a.dispatchEvent(evt);
-    } else {
-        // for IE
-        const html: string = `<body style="margin:0;">
-                    <img src="${base64}" style="max-width:100%;"  title="(test)" />
-                </body>`;
-        const tab: Window = window.open();
-        tab.document.write(html);
-    }
+    downloadBase64(base64, name);
 }
 
   public needUpdate(): boolean {
