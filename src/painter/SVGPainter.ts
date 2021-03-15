7dab8aaeff8ce6961d7a33ee44c83b0d441a2962
liwensheng.001 <liwensheng.001@bytedance.com>
2021-03-15 16:02:55 +0800

    fix svg remove node

--- src/painter/SVGPainter.ts
+++ src/painter/SVGPainter.ts
@@ -266,10 +266,13 @@ export default class SVGPainter implements Painter {
 
   private _removeNode(node: Element) {
     const el = this._loadedSVGElements[node.id];
+    if (!el.parentNode) {
+      console.warn(`remove node has no parentNode`, node);
+    }
     if (node.isClip) {
       el.parentNode.parentNode.removeChild(el.parentNode);
     } else {
-      el.parentNode.removeChild(el);
+      el.parentNode?.removeChild(el);
     }
     delete this._loadedSVGElements[node.id];
     node.clearDirty();
