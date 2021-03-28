73df15883dae63e55cad1ce5dd1f3db40b00dd0e
liwensheng.001 <liwensheng.001@bytedance.com>
2021-03-28 15:59:09 +0800

    pathlength

--- src/geometry/pathLength.ts
+++ src/geometry/pathLength.ts
@@ -1,26 +1,25 @@
 import Path2d, { PathAction } from './Path2D';
+import { getPointOnPolar, PI2, equalWithTolerance } from '../utils/math';
 
 interface Segment {
   action: PathAction['action'];
   length: number;
-  startLength: number;
-  endLength: number;
-  startPoint: [number, number];
-  endPoint: [number, number];
-  params: number;
+  startPoint?: [number, number];
+  params: any[];
 }
 
 
-export function getPathLength(path: Path2d): number {
+
+export function getPathSegments(path: Path2d, out: Segment[]): Segment[] {
   const pathList = path.getPathList();
   let startX: number;
   let startY: number;
   let endX: number;
   let endY: number;
-  let pathLegnth: number = 0;
   let action: PathAction['action'];
   let params: any[];
   let currentPath: PathAction;
+  let length: number;
   for (let i = 0; i < pathList.length; i++) {
     currentPath = pathList[i];
     action = currentPath.action;
@@ -33,12 +32,32 @@ export function getPathLength(path: Path2d): number {
     }
     if (action === 'lineTo') {
       const [x, y] = params[0];
-      pathLegnth += Math.sqrt()
+      length = Math.sqrt((x - endX) **2 + (y - endY) ** 2);
+      out.push({
+        action,
+        params,
+        length,
+        startPoint: [endX, endY],
+      });
       endX = x;
       endY = y;
     }
     if (action === 'arc') {
-
+      const [cx, cy, r, start, end, antiClockwise] = params;
+      const startPoint = getPointOnPolar(cx, cy, r, start);
+      const length = Math.abs(end - start) * r;
+      if (i > 0) {
+        out.push({
+          action: 'lineTo',
+          params: [startPoint.x, startPoint.y],
+          length: 0,
+        });
+      }
+      out.push({
+        action,
+        params,
+        length,
+      })
     }
 
     if (action === 'arcTo') {
@@ -46,18 +65,49 @@ export function getPathLength(path: Path2d): number {
     }
 
     if (action === 'bezierCurveTo') {
-
+      out.push({
+        action,
+        length: 0,
+        params,
+      })
     }
 
     if (action === 'rect') {
-
+      out.push({
+        action: 'lineTo',
+        length: 0,
+        params: [0, 1]
+      })
+      out.push({
+        action: 'lineTo',
+        length: 0,
+        params: [0, 1]
+      })
+      out.push({
+        action: 'lineTo',
+        length: 0,
+        params: [0, 1]
+      })
+      out.push({
+        action: 'lineTo',
+        length: 0,
+        params: [0, 1]
+      })
     }
     if (action === 'quadraticCurveTo') {
-
+      out.push({
+        action,
+        length: 0,
+        params,
+      })
     }
 
     if (action === 'closePath') {
-
+      out.push({
+        action: 'lineTo',
+        length: 0,
+        params,
+      })
     }
   }
 }
