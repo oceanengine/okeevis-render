395d856f63b1b1b802fa3e41a4b38b0a834e4061
liwensheng.001 <liwensheng.001@bytedance.com>
2021-07-28 18:20:41 +0800

    feat: image preserveAspectRatio support

--- src/shapes/Image.ts
+++ src/shapes/Image.ts
@@ -1,7 +1,8 @@
 import Shape from './Shape';
 import { CommonAttr } from './Element';
 import { getImage } from '../utils/imageLoader';
-import { BBox, rectBBox, inBBox, } from '../utils/bbox';
+import { BBox, rectBBox, inBBox, alignBox, BoxAlign, BoxVerticalAlign } from '../utils/bbox';
+
 
 export interface ImageConf extends CommonAttr {
   x?: number;
@@ -11,7 +12,24 @@ export interface ImageConf extends CommonAttr {
   width?: number;
   height?: number;
   src?: string;
+  /**
+   * https://developer.mozilla.org/zh-CN/docs/Web/SVG/Attribute/preserveAspectRatio
+   */
+  preserveAspectRatio?: 'none' | 'xMinYMin' | 'xMidYMin' | 'xMaxYMin' | 'xMinYMid' | 'xMidYMid' | 'xMaxYMid' | 'xMinYMax' | 'xMidYMax' | 'xMaxYMax';
 }
+
+const boxAlign: Record<Exclude<ImageConf['preserveAspectRatio'], 'none'>, [BoxAlign, BoxVerticalAlign]> = {
+  xMinYMin: ['left', 'top'],
+  xMidYMin: ['center', 'top'],
+  xMaxYMin: ['right', 'top'],
+  xMinYMid: ['left', 'middle'],
+  xMidYMid: ['center', 'middle'],
+  xMaxYMid: ['right', 'middle'],
+  xMinYMax: ['left', 'bottom'],
+  xMidYMax: ['center', 'bottom'],
+  xMaxYMax: ['right', 'bottom'],
+};
+
 const shapeKeys: Array<keyof ImageConf> = ['x', 'y', 'width', 'height', 'src'];
 
 export default class Image extends Shape<ImageConf> {
@@ -40,21 +58,33 @@ export default class Image extends Shape<ImageConf> {
   }
 
   public brush(ctx: CanvasRenderingContext2D) {
-    const { src, x, y, width, height } = this.attr;
+    const { src, preserveAspectRatio } = this.attr;
     if (!src) {
       return;
     }
     const image = getImage(src, () => {
       this.dirty();
     });
-    image &&
-      ctx.drawImage(
-        image,
-        x,
-        y,
-        width >= 0 ? width : image.width,
-        height >= 0 ? height : image.height,
-      );
+    if (image) {
+      const { x, y, width, height } = this._getImagePosition(image);
+      if (width === 0 || height === 0) {
+        return;
+      }
+      if (image.width && image.height && preserveAspectRatio !== 'none') {
+        ctx.drawImage(
+          image,
+          0, 0, image.width, image.height,
+          x,
+          y,
+          width >= 0 ? width : image.width,
+          height >= 0 ? height : image.height,
+        );
+      } else {
+        ctx.drawImage(image, x, y, width, height);
+      }
+
+    }
+
   }
 
   protected isPointOnPath(x: number, y: number): boolean {
@@ -69,7 +99,7 @@ export default class Image extends Shape<ImageConf> {
       width: this.attr.width,
       height: this.attr.height,
       "xlink:href": this.attr.src,
-      'preserveAspectRatio': 'none',
+      'preserveAspectRatio': this.attr.preserveAspectRatio || 'none',
     }
   }
 
@@ -80,6 +110,26 @@ export default class Image extends Shape<ImageConf> {
     const sh = height > 0 ? height : image?.height || 0;
     return rectBBox(x, y, sw, sh);
   }
-  
-  
+
+  private _getImagePosition(image: HTMLImageElement): BBox {
+    const { x, y, width, height, preserveAspectRatio = 'none' } = this.attr;
+    if (!preserveAspectRatio || preserveAspectRatio === 'none' || !image.width || !image.height) {
+      return { x, y, width, height };
+    }
+    const [align, verticalAlign] = boxAlign[preserveAspectRatio];
+    const imageWidth = image.width;
+    const imageHeight = image.height;
+    let outWidth = width;
+    let outHeight = height;
+    // 横向缩放，纵向铺满
+    if (width / height > imageWidth / imageHeight) {
+      outWidth = height *  imageWidth / imageHeight;
+    } else {
+      outHeight = width * imageHeight / imageWidth;
+    }
+    const { x: imageX, y: imageY } = alignBox({ x, y, width, height }, outWidth, outHeight, align, verticalAlign);
+    return { x: imageX, y: imageY, width: outWidth, height: outHeight };
+  }
+
+
 }
--- test/image.ts
+++ test/image.ts
@@ -2,11 +2,15 @@ import Render from '../src/render'
 import Image from '../src/shapes/Image'
 
 const dom = document.getElementById('root') as HTMLDivElement
-const render = new Render(dom)
+const render = new Render(dom, {renderer: 'svg'})
 
 const img = new Image({
   x: 0,
   y: 0,
-  src: 'https://lf1-hscdn-tos.pstatp.com/obj/developer-baas/baas/ttkw6x/140eef26a890065b_1601003200893.png'
+  width: 200,
+  height: 358,
+  showBBox: true,
+  src: 'https://lf1-hscdn-tos.pstatp.com/obj/developer-baas/baas/ttkw6x/140eef26a890065b_1601003200893.png',
+  preserveAspectRatio: 'xMidYMin',
 })
 render.add(img)
