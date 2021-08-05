6ef16aa103420670ad7453d88e5380155e1278eb
liwensheng.001 <liwensheng.001@bytedance.com>
2021-08-05 17:18:22 +0800

    use old bbox

--- src/painter/dirtyRect.ts
+++ src/painter/dirtyRect.ts
@@ -1,4 +1,4 @@
-import { BBox } from '../utils/bbox';
+import { BBox, unionBBox } from '../utils/bbox';
 
 /**  https://idom.me/articles/841.html
  * 1. 如果两个矩形合并后总面积小于两个矩形各自面积之和,则允许合并, 并且优先合并相交面积最大的两个矩形
@@ -7,8 +7,7 @@ import { BBox } from '../utils/bbox';
  * */
 export default function mergeDirtyRegions(mergedRects: BBox[]): BBox[] {
   mergedRects = mergedRects.filter(box => box.width > 0 && box.height > 0);
-  unionRect(mergedRects);
-  return mergedRects;
+  return [unionBBox(mergedRects)];
 }
 
 /**
