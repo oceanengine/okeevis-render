7b4a3b10f7e385d03b3052f4060b6feb194667d2
liwensheng.001 <liwensheng.001@bytedance.com>
2021-08-17 18:34:09 +0800

    event pick optimize

--- src/shapes/Group.ts
+++ src/shapes/Group.ts
@@ -390,16 +390,16 @@ export default class Group<T extends Element<any> = Element> extends Element<Gro
     return ret;
   }
 
-  public getAllLeafNodes(ret: Shape[], ignoreInvisible = false, ignoreMute = false): Shape[] {
+  public getAllLeafNodes(ret: Shape[], ignoreInvisible = false): Shape[] {
     this.eachChild(item => {
       const isGroup = item.isGroup;
       if (item.attr.display === false && ignoreInvisible) {
         return;
       }
-      if (!isGroup && !(item.getExtendAttr('pointerEvents') === 'none' && ignoreMute)) {
+      if (!isGroup) {
         ret.push((item as any) as Shape);
       } else if (isGroup) {
-        ((item as any) as Group).getAllLeafNodes(ret, ignoreInvisible, ignoreMute);
+        ((item as any) as Group).getAllLeafNodes(ret, ignoreInvisible);
       }
     });
     return ret;
