ecfe8037894dc880af83d3ba5e11bbf9998d51af
liwensheng.001 <liwensheng.001@bytedance.com>
2021-09-08 21:26:04 +0800

    gc optimize

--- src/shapes/Element.ts
+++ src/shapes/Element.ts
@@ -521,12 +521,13 @@ export default class Element<T extends CommonAttr = ElementAttr>
       return ceilBBox(this.getBoundingClientRect());
     }
     // 计算当前dirtyRect
-    const { x, y, width, height } = this.getBoundingClientRect();
+    const boundingRect = this.getBoundingClientRect();;
+    const { x, y, width, height } = boundingRect;
     // 暂不考虑miter尖角影响, 默认使用了bevel
     // const miterLimit = this.getExtendAttr('miterLimit');
     // const lineJoin = this.getExtendAttr('lineJoin');
     if (shadowBlur === 0) {
-      return ceilBBox({ x, y, width, height });
+      return ceilBBox(boundingRect);
     }
     const shadowOffsetX = this.getExtendAttr('shadowOffsetX');
     const shadowOffsetY = this.getExtendAttr('shadowOffsetY');
