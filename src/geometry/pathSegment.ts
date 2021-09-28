7f30047dd18b3081fb1b277e2be43fc379079c19
liwensheng.001 <liwensheng.001@bytedance.com>
2021-09-28 21:47:13 +0800

    code

--- src/geometry/pathSegment.ts
+++ src/geometry/pathSegment.ts
@@ -1,5 +1,5 @@
 import Path2d, { PathAction } from './Path2D';
-import { getPointOnPolar, PI2, equalWithTolerance } from '../utils/math';
+import { getPointOnPolar } from '../utils/math';
 
 interface Segment {
   type: 'line' | 'arc' | 'bezier';
@@ -63,30 +63,30 @@ function pointAtArc(t: number, cx: number, cy: number, r: number, startAngle: nu
 
 function pointAtBezier(t: number, p1x: number, p1y: number, c1x: number, c1y: number, c2x: number, c2y: number, p2x: number, p2y: number): SegmentPoint {
   var t1 = 1 - t,
-      t13 = Math.pow(t1, 3),
-      t12 = Math.pow(t1, 2),
-      t2 = t * t,
-      t3 = t2 * t,
-      x = t13 * p1x + t12 * 3 * t * c1x + t1 * 3 * t * t * c2x + t3 * p2x,
-      y = t13 * p1y + t12 * 3 * t * c1y + t1 * 3 * t * t * c2y + t3 * p2y,
-      mx = p1x + 2 * t * (c1x - p1x) + t2 * (c2x - 2 * c1x + p1x),
-      my = p1y + 2 * t * (c1y - p1y) + t2 * (c2y - 2 * c1y + p1y),
-      nx = c1x + 2 * t * (c2x - c1x) + t2 * (p2x - 2 * c2x + c1x),
-      ny = c1y + 2 * t * (c2y - c1y) + t2 * (p2y - 2 * c2y + c1y),
-      // ax = t1 * p1x + t * c1x,
-      // ay = t1 * p1y + t * c1y,
-      // cx = t1 * c2x + t * p2x,
-      // cy = t1 * c2y + t * p2y,
-      alpha = 90 - Math.atan2(mx - nx, my - ny) * 180 / Math.PI;
+    t13 = Math.pow(t1, 3),
+    t12 = Math.pow(t1, 2),
+    t2 = t * t,
+    t3 = t2 * t,
+    x = t13 * p1x + t12 * 3 * t * c1x + t1 * 3 * t * t * c2x + t3 * p2x,
+    y = t13 * p1y + t12 * 3 * t * c1y + t1 * 3 * t * t * c2y + t3 * p2y,
+    mx = p1x + 2 * t * (c1x - p1x) + t2 * (c2x - 2 * c1x + p1x),
+    my = p1y + 2 * t * (c1y - p1y) + t2 * (c2y - 2 * c1y + p1y),
+    nx = c1x + 2 * t * (c2x - c1x) + t2 * (p2x - 2 * c2x + c1x),
+    ny = c1y + 2 * t * (c2y - c1y) + t2 * (p2y - 2 * c2y + c1y),
+    // ax = t1 * p1x + t * c1x,
+    // ay = t1 * p1y + t * c1y,
+    // cx = t1 * c2x + t * p2x,
+    // cy = t1 * c2y + t * p2y,
+    alpha = 90 - Math.atan2(mx - nx, my - ny) * 180 / Math.PI;
   // (mx > nx || my < ny) && (alpha += 180);
   return {
-      x: x,
-      y: y,
-      // m: {x: mx, y: my},
-      // n: {x: nx, y: ny},
-      // start: {x: ax, y: ay},
-      // end: {x: cx, y: cy},
-      alpha: alpha
+    x: x,
+    y: y,
+    // m: {x: mx, y: my},
+    // n: {x: nx, y: ny},
+    // start: {x: ax, y: ay},
+    // end: {x: cx, y: cy},
+    alpha: alpha
   };
 }
 
