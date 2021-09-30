66cb9319ec4df313ec1381029f91d989fabeccb5
liwensheng.001 <liwensheng.001@bytedance.com>
2021-09-30 15:12:22 +0800

    marker

--- test/marker.ts
+++ test/marker.ts
@@ -5,12 +5,11 @@ import Marker from '../src/shapes/Marker';
 import Path from '../src/shapes/Path'
 
 const dom = document.getElementById('root') as HTMLDivElement
-const render = new Render(dom, {renderer: 'svg'})
+const render = new Render(dom, {renderer: 'canvas'})
 render.enableDirtyRect = true;
 
 const arrow = Path.fromSvgPath("M 0 0 L 10 5 L 0 10 z").setAttr({
   fill: 'red',
-  fillOpacity: 0.5,
 })
 
 const rect = new Rect({
@@ -38,12 +37,12 @@ const line = new Line({
   y2: 300,
   draggable: true,
   markerEnd: marker,
-  markerMid: marker,
   markerStart: marker,
   stroke: 'blue',
   lineWidth: 1,
   lineCap: 'butt',
   origin: [300, 300],
+  pickingBuffer: 60
 })
 
 render.add(line)
