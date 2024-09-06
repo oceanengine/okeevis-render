616ec0d0d085fcbdc4ebd3d07099a5ee9a655be2
liwensheng.001 <liwensheng.001@bytedance.com>
2024-09-06 17:25:45 +0800

    feat: createRoot render

--- src/utils/dom-renderer.ts
+++ src/utils/dom-renderer.ts
@@ -1,18 +1,14 @@
-
-
 const rendererStorage: Record<string, DOMRenderer> = {};
-
-
 
 export type DOMRenderer = {
   update: (dom: HTMLDivElement, content: unknown) => void;
   destroy: (dom: HTMLDivElement) => void;
-}
+};
 
 rendererStorage.html = {
-  update: (dom, content) => dom.innerHTML = content + '',
-  destroy: dom => dom.innerHTML = '',
-}
+  update: (dom, content) => (dom.innerHTML = content + ''),
+  destroy: dom => (dom.innerHTML = ''),
+};
 
 export function getDOMRenderer(type: string) {
   return rendererStorage[type];
@@ -22,9 +18,31 @@ export function registerDOMRenderer(type: string, renderer: DOMRenderer) {
   rendererStorage[type] = renderer;
 }
 
-export function createReactRenderer(ReactDOM: { render: Function, unmountComponentAtNode: Function }): DOMRenderer {
+export function createReactRenderer(ReactDOM: {
+  render: Function;
+  unmountComponentAtNode: Function;
+  version: string;
+  createRoot: any;
+}): DOMRenderer {
+  if (ReactDOM.createRoot) {
+    let app: any;
+    return {
+      update: (dom, content) => {
+        if (!app) {
+          app = ReactDOM.createRoot(dom);
+        }
+        app.render(content);
+      },
+      destroy: dom => {
+        if (app) {
+          app.unmount();
+          app = null;
+        }
+      },
+    };
+  }
   return {
     update: (dom, content) => ReactDOM.render(content, dom),
     destroy: dom => ReactDOM.unmountComponentAtNode(dom),
-  }
+  };
 }
